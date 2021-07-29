d915118d279ab2582aa6a5bc3e2ce17b35ad62de
Ivan Diaz <ivan@opensupports.com>
2021-07-29 18:47:08 -0300

    Add search button

--- src/components/PoolSearcher.tsx
+++ src/components/PoolSearcher.tsx
@@ -6,7 +6,7 @@ import './PoolSearcher.css';
 
 function PoolSearcher() {
   const dispatch = useAppDispatch();
-  const [value, setValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const [opened, setOpened] = useState(false);
   const pools = useAppSelector(state => state.app.suggestedPools);
   const loading = useAppSelector(state => state.app.suggestedPoolsLoading);
@@ -17,8 +17,14 @@ function PoolSearcher() {
   }, [contractAddress]);
 
   let results = !loading ?
-    pools.filter(pool => pool.startsWith(value)).map(pool => ({value: pool})) :
+    pools.filter(pool => pool.startsWith(inputValue)).map(pool => ({value: pool})) :
     [{value: <div>Loading pools... <Loader active inline /></div>}];
+
+  const searchPool = (value) => {
+    dispatch(loadPool(value));
+    setInputValue(value);
+    setOpened(false);
+  };
 
   return (
     <div className="PoolSearcher" data-loading={loading}>
@@ -28,17 +34,15 @@ function PoolSearcher() {
         fluid
         loading={loading}
         placeholder="Enter pool id..."
-        onSearchChange={(_, {value}) => setValue(value)}
-        onResultSelect={(e, data) => {
-          dispatch(loadPool(data.result.value));
-          setOpened(false)
-        }}
+        onSearchChange={(_, {value}) => setInputValue(value)}
+        onResultSelect={(e, data) => searchPool(data.result.value)}
         onBlur={() => setOpened(false)}
         onFocus={() => setOpened(true)}
         open={opened}
         resultRenderer={({ value }) => <div key={value}>{value}</div>}
         results={results}
-        value={value} />
+        value={inputValue} />
+      <Button content='Search' size="small" style={{marginLeft: 10}} onClick={() => searchPool(inputValue)}/>
     </div>
   );
 }
